11555af443945c828f7085bbdea3ca5de9e999bb
Jamshid <mamatovjamshid506@gmail.com>
2021-02-07 22:36:16 +0500

    like and dislike count user

--- main.py
+++ main.py
@@ -3,39 +3,45 @@ import os
 token=os.environ['token']
 
 inform={}
-inform['like']=0
-inform['dislike']=0
+like=0
+dislike=0
+
 def update():
     url=f"https://api.telegram.org/bot{token}/getUpdates"
     
     r=requests.get(url)
-    like=0
-    dislike=0
-    data=r.json()
-
-def sendphoto(chat_id,message_photo,msg_id):
-    url=f"https://api.telegram.org/bot{token}/sendPhoto"
-
-    payload={
-        'chat_id':chat_id,
-        'photo':message_photo,
-        'reply_to_message_id':msg_id
-    }
-    r=requests.get(url,params=payload)
-
-
-def update_id():
-    url=f"https://api.telegram.org/bot{token}/getUpdates"
     
-    r=requests.get(url)
     data=r.json()
-    id=data['result'][-1]['update_id']
-    return id
+    message=data['result'][-1]['message']
+    text=message.get('text')
+    update_id=data['result'][-1]['update_id']
+    from_id=data['result'][-1]['message']['from']['id']
+    lst=[text,update_id,from_id]
+    return lst
 
-    
+ 
 last_update_id=None
 while True:
 
-    new_update_id=update_id()
+    lst=update()
+    new_update_id=lst[1]
+    
     if last_update_id!=new_update_id:
-        update()
+        last_update_id=new_update_id
+        id=lst[2]
+        text=lst[0]
+
+        if inform.get(id)==None:
+            inform[id]={'like':0,
+                        'dislike':0
+                        }
+        elif text!=None:
+            if text=='👎':
+                dislike+=1
+            elif text=="👍":
+                like+=1
+            
+            inform[id]['like']=like
+            inform[id]['dislike']=dislike
+
+     
