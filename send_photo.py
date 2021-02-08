f9446161a14d7d2ebd612fd0810dc9dd3a448d2f
Jamshid <mamatovjamshid506@gmail.com>
2021-02-08 11:20:14 +0500

    send photo with file

--- send_photo.py
+++ send_photo.py
@@ -6,12 +6,14 @@ from pprint import pprint
 #https://random.dog/5c9f8baf-7918-4545-bd86-8f441cf96f3e.jpg
 def sendphoto(ids):
     url=f"https://api.telegram.org/bot{token}/sendPhoto"
+    photo=open("logo.jpg",'rb')
+
     payload={
         'chat_id':ids,
-        'photo':'AgACAgQAAxkDAAOYYCDQWwWr6EBZGYRvKx04mxPHFzIAAgWqMRsAAZGsUv_7fqrhaJ5KR2OoGwAEAQADAgADeAADtisBAAEeBA',
+        
         'caption':"send phot file_id"
     }
-    r=requests.get(url,params=payload)
+    r=requests.get(url,params=payload,files={'photo':photo})
     return r.json()
     
 idx=937691492
