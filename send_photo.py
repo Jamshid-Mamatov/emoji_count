9ee780d050b82d106e2c978490f47da72f3aa60a
Jamshid <mamatovjamshid506@gmail.com>
2021-02-08 10:48:37 +0500

    send photo with title

--- send_photo.py
+++ send_photo.py
@@ -8,7 +8,8 @@ def sendphoto(ids):
     url=f"https://api.telegram.org/bot{token}/sendPhoto"
     payload={
         'chat_id':ids,
-        'photo':'https://random.dog/5c9f8baf-7918-4545-bd86-8f441cf96f3e.jpg'
+        'photo':'https://random.dog/5c9f8baf-7918-4545-bd86-8f441cf96f3e.jpg',
+        'caption':"title"
     }
     r=requests.get(url,params=payload)
     return r.json()
