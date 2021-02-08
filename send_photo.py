c16756050a37248f53cc5c92b7593bf66eebfbb6
Jamshid <mamatovjamshid506@gmail.com>
2021-02-08 10:40:54 +0500

    send photo

---
+++ send_photo.py
@@ -0,0 +1,18 @@
+import requests
+import os
+token=os.environ['token']
+from pprint import pprint
+
+#https://random.dog/5c9f8baf-7918-4545-bd86-8f441cf96f3e.jpg
+def sendphoto(ids):
+    url=f"https://api.telegram.org/bot{token}/sendPhoto"
+    payload={
+        'chat_id':ids,
+        'photo':'https://random.dog/5c9f8baf-7918-4545-bd86-8f441cf96f3e.jpg'
+    }
+    r=requests.get(url,params=payload)
+    return r.json()
+    
+idx=937691492
+
+pprint(sendphoto(idx))
